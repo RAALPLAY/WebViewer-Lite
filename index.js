3a1364ea5fb31ec3621059fd0aa8b1f480d7338c
Rafael Hernández <84200771+RAALPLAY@users.noreply.github.com>
2023-01-23 23:52:01 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 100 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+$(document).ready(function(){
+    $.get("update.xml",{},function(xml){
+      $('update',xml).each(function(i) {
+        var url = ((xml.getElementsByTagName('link')[i]).childNodes[0]).nodeValue;
+        var version = ((xml.getElementsByTagName('version')[i]).childNodes[0]).nodeValue;
+        var info = ((xml.getElementsByTagName('info')[i]).childNodes[0]).nodeValue;
+        document.getElementById("ver").innerHTML = "v" + version
+        document.getElementById("download").href = url
+
+        $.getJSON(info, function(data){
+        var download_count = data.assets[0].download_count
+        document.getElementById("download-count").innerHTML = download_count
+      });
+      });
+    });
+});
